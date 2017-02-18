1f2229f3cae96374b54bfe975f4e8ec31fc1ca26
leevely <yooo0220@nate.com>
2017-02-18 14:48:01 +0900

    tab2가지 완료 (페이드추가)

--- js/main.js
+++ js/main.js
@@ -1,52 +1,18 @@
 'use strict';
 $(document).ready(function () {
 
-    var tab = new TabMenu(); //변수 tab에 new TabMenu라는 클래스(하단)를 넣어라(복사하라)
-                              //플러그인의 개념
+     //변수 tab에 new TabMenu라는 클래스(하단)를 넣어라(복사하라)
+    //플러그인의 개념
+
+    var tab1 = new TabMenu({
+        selector: ".tab1"            //객체데이터
+    });
+
+    var tab2 = new TabMenu({
+        selector: ".tab2",
+        fade: true,     //값을 안적으면 undefined가 들어감 -> false로 변환됨
+        duration: 400 // fade되는 속도
+    });
+
 });
 
-
-function TabMenu() {  //TabMenu라는 클래스임
-    this.init();     // this는 TabMenu를 의미!
-    this.initEvent();
-}
-
-TabMenu.prototype = {
-
-    // 구조를 정리
-    init: function () {   // init는 메소드
-        this.$tabMenu = $(".tab_menu li");
-        this.$tabContents = $(".tab_contents li");
-        this.oldIndex = null;
-        this.currentIndex = 0;
-    },
-
-    // 시작하자마자 실행할 이벤트들   $(this) : 클릭한메뉴
-    initEvent: function () {
-        var _this = this; //this는 탭메뉴라는 클래스를 의미함
-
-        this.$tabMenu.on("click", function () {
-            _this.renewIndex( $(this) );
-            _this.toggleMenu( $(this) );
-            _this.toggleContent();
-        });
-    },
-
-    // 번호 갱신 기능
-    renewIndex: function ($this) {
-        this.oldIndex = this.currentIndex;
-        this.currentIndex = $this.index();
-    },
-
-    // 메뉴 변경 기능
-    toggleMenu: function ($this) {
-        this.$tabMenu.not($this).removeClass("active");
-        $this.addClass("active");
-    },
-
-    // 이미지 변경 기능
-    toggleContent: function () {
-        this.$tabContents.eq(this.oldIndex).hide();
-        this.$tabContents.eq(this.currentIndex).show();
-    }
-};
